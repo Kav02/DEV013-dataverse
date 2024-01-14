64d1e88c9c129a68b8b6d8a5eee4d127aa21f714
Diana <educ.dana@gmail.com>
2024-01-14 00:01:22 -0500

    prueba

--- src/view.js
+++ src/view.js
@@ -1,4 +1,4 @@
-export const renderItems = (data) => {
+git git export const renderItems = (data) => {
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
   return 'example';
