7f0d6f1503370266b79fe2b84c3982442c3f37fb
Korin <kav02@outlook.com>
2024-01-15 15:07:40 -0600

    Crear tarjetas y Agregar CSS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { anotherExample } from './dataFunctions.js';
 import { renderItems } from './view.js';
-
 import data from './data/artdata.js';
 
+const artWorkList = document.getElementById('artWorkList');
+const sectionItems = renderItems(data);
+artWorkList.appendChild(sectionItems);
 
-
-console.log(anotherExample, renderItems(data), data);
+console.log(renderItems(data));
