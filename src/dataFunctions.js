587e44ee54308fdd9654e046479044bd60b3fc33
Korin <kav02@outlook.com>
2024-01-17 10:47:06 -0600

    Datos de las obras y url corregidos, seudocodigo en datafunctions

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -3,3 +3,9 @@
 export const anotherExample = () => {
   return [];
 };
+export const filterData  = () =>{
+//data, filterBy, value
+//Crear filtro por artista
+//Crear un array con los artistas para desplegar la lista para seleccionar
+//Crear un evento para que al hacer click en el artista recorra la base de datos y cuando encuentre coincidencia despliegue la tarjeta en pantalla
+};
