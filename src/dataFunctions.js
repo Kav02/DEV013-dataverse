841343cb52dcdbeba0155d347b486d06baf1c9a5
Korin <kav02@outlook.com>
2024-01-17 17:55:07 -0600

    Comentarios en el codigo

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -3,9 +3,17 @@
 export const anotherExample = () => {
   return [];
 };
-export const filterData  = () =>{
+export const filterData  = () =>{};
 //data, filterBy, value
-//Crear filtro por artista
-//Crear un array con los artistas para desplegar la lista para seleccionar
+//Crear filtro por artista en main
+
+//Crear un desplegable recorriendo la información del artdata buscando los artistas para desplegar la lista para que el usario seleccione
 //Crear un evento para que al hacer click en el artista recorra la base de datos y cuando encuentre coincidencia despliegue la tarjeta en pantalla
+//  1-Buscar en cada tarjeta el nombre del artista y cuando coincide despliega la tarjeta en pantalla, mostrando todas las que tienen coincidencia
+//  2-Al dar click en el botón limpiar regresa a "home"
+//  3- Ordenar (desarrollar aparte) 
+
+  //Crear orden
+
+
 };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
-import { anotherExample } from './dataFunctions.js';
-import { renderItems } from './view.js';
-import data from './data/artdata.js';
+import { anotherExample } from "./dataFunctions.js";
+import { renderItems } from "./view.js";
+import data from "./data/artdata.js";
 
-const artWorkList = document.getElementById('artWorkList');
+//Genera las tarjetas a partir de render items
+const artWorkList = document.getElementById("artWorkList");
 const sectionItems = renderItems(data);
 artWorkList.appendChild(sectionItems);
 
-console.log(renderItems(data));
+//Crea la lista de artistas
+const artistList = [];
+for (const artwork of data) {
+  const artistName = artwork.facts.artistName;
+  if (!artistList.includes(artistName)) {//Incluye solo los nombres diferentes
+    artistList.push(artistName); //Push para agregar en el array
+  }
+}
+console.log(artistList);
+
+//
+const artistOptions = artistList.map((artist) => `<option value="${artist}">${artist}</option>`);
+const artistSelect = document.getElementById("artist-filter");
+artistSelect.innerHTML = `
+  <option value="">Todos los artistas</option>
+  ${artistOptions.join("")}
+`;
