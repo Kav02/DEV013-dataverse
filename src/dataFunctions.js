a6279f134424212ce1f03f94f5689bc82a236ec8
Korin <kav02@outlook.com>
2024-01-29 11:28:08 -0600

    Agregar filtro artistas

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,20 +1,18 @@
-// Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
-
-export const anotherExample = () => {
-  return [];
+export const filterData = (data, filterBy, value) => {
+  /*Crear filtro por artista en main
+Crear un desplegable recorriendo la información del artdata buscando los artistas para desplegar la lista para que el usario seleccione*/
+  //Crear un evento para que al hacer click en el artista recorra la base de datos y cuando encuentre coincidencia despliegue la tarjeta en pantalla
+  //  1-Buscar en cada tarjeta el nombre del artista y cuando coincide despliega la tarjeta en pantalla, mostrando todas las que tienen coincidencia
+  const paintingsperFilter = [];
+  for (const painting of data) {
+    if (painting.facts[filterBy] === value) {
+      paintingsperFilter.push(painting);
+    }
+  }
+  return paintingsperFilter;
+  
 };
-export const filterData = () => {};
-//data, filterBy, value
-//Crear filtro por artista en main
 
-//Crear un desplegable recorriendo la información del artdata buscando los artistas para desplegar la lista para que el usario seleccione
-//Crear un evento para que al hacer click en el artista recorra la base de datos y cuando encuentre coincidencia despliegue la tarjeta en pantalla
-//  1-Buscar en cada tarjeta el nombre del artista y cuando coincide despliega la tarjeta en pantalla, mostrando todas las que tienen coincidencia
-//  2-Al dar click en el botón limpiar regresa a "home"
-//  3- Ordenar (desarrollar aparte)
-//Crear orden
-
-//};
 
 export const sortData = (data, sortBy, sortOrder) => {
   const nombre = [];
--- src/main.js
+++ src/main.js
@@ -1,72 +1,86 @@
-// import { anotherExample } from './dataFunctions.js';
+import { filterData } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
-import { renderCards } from "./view.js";
 import data from "./data/artdata.js";
 
 // Genera las tarjetas a partir de renderItems
-const artWorkList = document.getElementById("root");
+const artWorkList = document.querySelector("#root");
 const shortCards = renderItems(data); // Agrega las tarjetas al elemento artWorkList
 artWorkList.appendChild(shortCards);
-
 
 //Crea la lista de artistas
 const artistList = []; //Crear array vacio
 for (const artwork of data) {
   const artistName = artwork.facts.artistName;
-  if (!artistList.includes(artistName)) {    //Incluye solo los nombres diferentes
+  if (!artistList.includes(artistName)) {
+    //Incluye solo los nombres diferentes
     artistList.push(artistName); //Push para agregar en el array
+    artistList.sort();
   }
 }
-
 //Incluir artistas en las opciones de filtrado
 const artistOptions = artistList.map(
   //Una vez obtenidos los nombres de los artistas con map se rellenan las opciones
-  (artist) => `<option value="${artist}">${artist}</option>`//Crea cada opcion
+  (artist) => `<option value="${artist}">${artist}</option>` //Crea cada opcion
 );
-const artistSelect = document.getElementById("artist-filter");//Inserta las opciones en el html
+const artistSelect = document.querySelector("#artist-filter"); //Inserta las opciones en el html
 artistSelect.innerHTML = `
-  <option value="">Artistas</option>
-  ${artistOptions.join("")}
-`;
+  <option disable selected>Artista</option>
+  ${artistOptions.join("")}`;
 
+//Crea la lista de Corrientes
+const movementList = []; //Crear array vacio
+for (const artwork of data) {
+  const movement = artwork.facts.artMovement;
+  if (!movementList.includes(movement)) {
+    //Incluye solo las corrientes diferentes
+    movementList.push(movement); //Push para agregar en el array
+    movementList.sort();
+  }
+}
+// Incluir lista en las opciones de filtrado
+const movementOptions = movementList.map(
+  (move) => `<option value="${move}">${move}</option>`
+);
+const movementSelect = document.getElementById("artmovent-filter");
+movementSelect.innerHTML = `<option value="">Corrientes</option>
+${movementOptions.join("")}`;
 
+//Función de filtrar
+//Obtener el artista seleccionado
+document.querySelector("#artist-filter")
+  .addEventListener("change", function (event) {
+    const artistDisplay = event.target.value;
+    const artistCards = filterData(data, "artistName", artistDisplay);
+    const filteredCards = renderItems(artistCards);
+    const clearScreen = document.getElementById("root");
+    clearScreen.innerHTML= "";
+    clearScreen.appendChild(filteredCards); 
+  });
+
+//Botón limpiar
+function clear_filters() {
+  const clearScreen = document.getElementById('root');
+  clearScreen.innerHTML= ""; 
+  clearScreen.appendChild(shortCards);
+  document.getElementById("artist-filter").value = 'Artista';
+  document.getElementById("movement-filter").value = 'Corriente';
+}
+const buttonClear = document.querySelector('[data-testid="button-clear"]');
+buttonClear.addEventListener("click", clear_filters);
+
+/*No se está usando
 const artName = [];
 for (const list of data) {
   const artworkName = list.name;
   artName.push(artworkName);
   artName.sort();
 }
-console.log(artName);
+console.log(artName);*/
 
-
-//Crea la lista de Corrientes
-const MovementList = []; //Crear array vacio
-for (const artwork of data) {
-  const Movement = artwork.facts.artMovement;
-  if (!MovementList.includes(Movement)) {
-    //Incluye solo las corrientes diferentes
-    MovementList.push(Movement); //Push para agregar en el array
-  }
-}
-console.log(MovementList);
-
-// Incluir lista en las opciones de filtrado
-const movementOptions = MovementList.map( Move =>`<option value="${Move}">${Move}</option>`);
-const Movementselect= document.getElementById("artmovent-filter");
-Movementselect.innerHTML =
-`<option value="">Corrientes</option>
-
-${movementOptions.join("")}
-`;
-
-const artmovement = [];
+/*const artMovement = [];
 for (const list of data) {
-  const artworkMovement = list.name;
-  artmovement.push(artworkMovement);
-  artmovement.sort();
+  const artworkMovement = list.artMovement;
+  artMovement.push(artworkMovement);
+  artMovement.sort();
 }
-console.log(artmovement);
-
-
-
-// console.log(movementOptions);
+console.log(artMovement);*/
