833337f4b993290db0ce8f1494aa536786bd78b8
Korin <kav02@outlook.com>
2024-01-30 09:49:11 -0600

    Arreglar el sort

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -10,43 +10,25 @@ Crear un desplegable recorriendo la información del artdata buscando los artist
     }
   }
   return paintingsperFilter;
-  
 };
-
-//data, filterBy, value
-//Crear filtro por artista en main
-
-//Crear un desplegable recorriendo la información del artdata buscando los artistas para desplegar la lista para que el usario seleccione
-//Crear un evento para que al hacer click en el artista recorra la base de datos y cuando encuentre coincidencia despliegue la tarjeta en pantalla
-//  1-Buscar en cada tarjeta el nombre del artista y cuando coincide despliega la tarjeta en pantalla, mostrando todas las que tienen coincidencia
-//  2-Al dar click en el botón limpiar regresa a "home"
-//  3- Ordenar (desarrollar aparte)
-//Crear orden
-
-
-
-export const filterData = (data, filterBy, value) => {
-  /*Crear filtro por artista en main
-Crear un desplegable recorriendo la información del artdata buscando los artistas para desplegar la lista para que el usario seleccione*/
-  //Crear un evento para que al hacer click en el artista recorra la base de datos y cuando encuentre coincidencia despliegue la tarjeta en pantalla
-  //  1-Buscar en cada tarjeta el nombre del artista y cuando coincide despliega la tarjeta en pantalla, mostrando todas las que tienen coincidencia
-  const paintingsperFilter = [];
-  for (const painting of data) {
-    if (painting.facts[filterBy] === value) {
-      paintingsperFilter.push(painting);
-    }
-  }
-  return paintingsperFilter;
-}
-
-//-------------------------------------------------
 
 export const sortData = (data, sortBy, sortOrder) => {
-  const nombre = [];
+  const sortedCard = [];
   for (const list of data) {
-    const name = list.name;
-    nombre.push(list);
-    nombre.sort();
+    sortedCard.push(list);}
+  if (sortOrder === "asc") {
+    sortedCard.sort((a, b) => a.name.localeCompare(b.name));
+  } else {sortOrder === "desc";
+    sortedCard.sort((a, b) => b.name.localeCompare(a.name));
   }
-  console.log(nombre);
+  return sortedCard;
 };
+
+/*No se está usando
+const artName = [];
+for (const list of data) {
+  const artworkName = list.name;
+  artName.push(artworkName);
+  artName.sort();
+}
+console.log(artName);*/
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { filterData } from "./dataFunctions.js";
+import { sortData } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
 import data from "./data/artdata.js";
-import {filterData} from "./dataFunctions.js";
 
 // Genera las tarjetas a partir de renderItems
 const artWorkList = document.querySelector("#root");
@@ -25,7 +25,7 @@ const artistOptions = artistList.map(
 );
 const artistSelect = document.querySelector("#artist-filter"); //Inserta las opciones en el html
 artistSelect.innerHTML = `
-  <option disable selected>Artista</option>
+  <option disabled selected>Artistas</option>
   ${artistOptions.join("")}`;
 
 //Crea la lista de Corrientes
@@ -39,38 +39,64 @@ for (const artwork of data) {
   }
 }
 // Incluir lista en las opciones de filtrado
-
 const movementOptions = movementList.map(
   (move) => `<option value="${move}">${move}</option>`
 );
-const movementSelect = document.getElementById("artmovent-filter");
-movementSelect.innerHTML = `<option value="">Corrientes</option>
+const movementSelect = document.getElementById("artmovement-filter");
+movementSelect.innerHTML = `<option disabled selected>Corrientes</option>
 ${movementOptions.join("")}`;
-
 
 //Función de filtrar
 //Obtener el artista seleccionado
-document.querySelector("#artist-filter")
-  .addEventListener("change", function (event) {
-    const artistDisplay = event.target.value;
-    const artistCards = filterData(data, "artistName", artistDisplay);
-    const filteredCards = renderItems(artistCards);
-    const clearScreen = document.getElementById("root");
-    clearScreen.innerHTML= "";
-    clearScreen.appendChild(filteredCards); 
-  });
+artistSelect.addEventListener("change", function (event) {
+  const artistDisplay = event.target.value;
+  const artistCards = filterData(data, "artistName", artistDisplay);
+  const filteredCards = renderItems(artistCards);
+  const clearScreen = document.getElementById("root");
+  clearScreen.innerHTML = "";
+  clearScreen.appendChild(filteredCards);
+});
 
+//OBTENER FILTRADO POR CORRIENTES
+
+//extraer o agruparlas las tarjetas con la corriente selecciona
+//sin recargar la pagina , volver a la pagina vacia
+//colocar las tarjetas seleccionadas
+
+movementSelect.addEventListener("change", function () {
+  //change:se dispara cuando hay una alteración para <select> al valor de un elemento es confirmada por el usuario.
+  const selectedArtMovement = movementSelect.value;
+  // Filtrar y mostrar las tarjetas correspondientes
+  const filterMovement = filterData(data, "artMovement", selectedArtMovement);
+  const filteredCards = renderItems(filterMovement);
+  const rootfilterMovement = document.getElementById("root");
+  rootfilterMovement.innerHTML = ""; // .innerHTML = "" :se limpia el contenedor antes de agregar nuevas tarjetas
+  // Agregar las tarjetas filtradas al contenedor
+  rootfilterMovement.appendChild(filteredCards);
+});
 
 //Botón limpiar
 function clear_filters() {
-  const clearScreen = document.getElementById('root');
-  clearScreen.innerHTML= ""; 
+  const clearScreen = document.getElementById("root");
+  clearScreen.innerHTML = "";
   clearScreen.appendChild(shortCards);
-  document.getElementById("artist-filter").value = 'Artista';
-  document.getElementById("movement-filter").value = 'Corriente';
+  document.getElementById("artmovement-filter").value = "Corrientes";
+  document.getElementById("artist-filter").value = "Artistas";
 }
 const buttonClear = document.querySelector('[data-testid="button-clear"]');
 buttonClear.addEventListener("click", clear_filters);
+
+//Ordenar alfabeticamente
+document
+  .querySelector("#alphabetical-order")
+  .addEventListener("change", function (event) {
+    const sortOrder = event.target.value;
+    const sortItems = sortData(data, "Ordenar", sortOrder);
+    const sortedCards = renderItems(sortItems);
+    const rootSorted = document.getElementById("root");
+    rootSorted.innerHTML = ""; // .innerHTML = "" :se limpia el contenedor antes de agregar nuevas tarjetas
+    rootSorted.appendChild(sortedCards);
+  });
 
 /*No se está usando
 const artName = [];
@@ -88,31 +114,3 @@ for (const list of data) {
   artMovement.sort();
 }
 console.log(artMovement);*/
-=======
-const movement = [];
-for (const list of data) {
-  const artworkMovement = list.name;
-  movement.push(artworkMovement);
-  movement.sort();
-}
-console.log(movement);
-
-//OBTENER FILTRADO POR CORRIENTES
-
-//extraer o agruparlas las tarjetas con la corriente selecciona
-//sin recargar la pagina , volver a la pagina vacia
-//colocar las tarjetas seleccionadas
-
-movementSelect.addEventListener("change", function () {   //change:se dispara cuando hay una alteración para <select> al valor de un elemento es confirmada por el usuario.
-  const selectedArtMovement = movementSelect.value;
-  // Filtrar y mostrar las tarjetas correspondientes
-  const filterMovement = filterData(data, "artMovement", selectedArtMovement);
-
-  const filteredCards = renderItems(filterMovement);
-
-  const rootfilterMovement = document.getElementById("root");
-  rootfilterMovement.innerHTML = "";      // .innerHTML = "" :se limpia el contenedor antes de agregar nuevas tarjetas
-  // Agregar las tarjetas filtradas al contenedor
-  rootfilterMovement.appendChild(filteredCards);
-});
-
